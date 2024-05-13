19111effa0dd80c9fd1c05de4c0a03fa82fa75df
Lucas Cardone <lucas.a.cardone@gmail.com>
2024-05-13 08:21:00 -0300

    Agregando doc. Swagger

---
+++ src/main/java/com/example/buensaboruno/domain/dtos/articuloDTO/ArticuloManufacturadoShortDTO.java
@@ -0,0 +1,7 @@
+package com.example.buensaboruno.domain.dtos.articuloDTO;
+
+public class ArticuloManufacturadoShortDTO extends ArticuloDTO{
+    private String descripcion;
+    private Integer tiempoEstimadoMinutos;
+    private String preparacion;
+}
