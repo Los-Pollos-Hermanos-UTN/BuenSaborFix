065730eaab27371ea6d6b7ed7765d5d0985ab3f6
francosantzz <franco.santibanez@alumnos.frm.utn.edu.ar>
2024-06-06 17:28:16 -0300

    find cliente by usuario

--- src/main/java/com/example/buensaboruno/business/services/impl/UsuarioClienteServiceImpl.java
+++ src/main/java/com/example/buensaboruno/business/services/impl/UsuarioClienteServiceImpl.java
@@ -1,20 +1,48 @@
 package com.example.buensaboruno.business.services.impl;
 
+import com.example.buensaboruno.business.mapper.ClienteMapper;
 import com.example.buensaboruno.business.services.UsuarioClienteService;
 import com.example.buensaboruno.business.services.base.BaseServiceImpl;
+import com.example.buensaboruno.domain.dtos.ClienteDTO;
+import com.example.buensaboruno.domain.entities.Cliente;
 import com.example.buensaboruno.domain.entities.UsuarioCliente;
+import com.example.buensaboruno.repositories.ClienteRepository;
 import com.example.buensaboruno.repositories.base.BaseRepository;
 import com.example.buensaboruno.repositories.UsuarioClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UsuarioClienteServiceImpl extends BaseServiceImpl<UsuarioCliente, Long> implements UsuarioClienteService {
     @Autowired
     private UsuarioClienteRepository usuarioClienteRepository;
 
-    public UsuarioClienteServiceImpl(BaseRepository<UsuarioCliente, Long> baseRepository, UsuarioClienteRepository usuarioClienteRepository) {
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ClienteMapper clienteMapper;
+
+    public UsuarioClienteServiceImpl(BaseRepository<UsuarioCliente, Long> baseRepository, UsuarioClienteRepository usuarioClienteRepository, ClienteRepository clienteRepository, ClienteMapper clienteMapper) {
         super(baseRepository);
-        this.usuarioClienteRepository=usuarioClienteRepository;
+        this.usuarioClienteRepository = usuarioClienteRepository;
+        this.clienteRepository = clienteRepository;
+        this.clienteMapper = clienteMapper;
     }
+
+    @Override
+    public ClienteDTO getClienteByUsuario(Long id) {
+        Optional<UsuarioCliente> usuarioClienteOpt = usuarioClienteRepository.findById(id);
+        if (usuarioClienteOpt.isPresent()) {
+            UsuarioCliente usuarioCliente = usuarioClienteOpt.get();
+            Cliente cliente = clienteRepository.findByUsuario(usuarioCliente);
+            if (cliente != null) {
+                return clienteMapper.toDTO(cliente);
+            }
+        }
+        return null; // O lanzar una excepción según tu manejo de errores
+    }
+
 }
--- src/main/java/com/example/buensaboruno/presentation/rest/UsuarioClienteController.java
+++ src/main/java/com/example/buensaboruno/presentation/rest/UsuarioClienteController.java
@@ -1,18 +1,33 @@
 package com.example.buensaboruno.presentation.rest;
 
 import com.example.buensaboruno.business.facade.impl.UsuarioClienteFacadeImpl;
+import com.example.buensaboruno.business.services.impl.UsuarioClienteServiceImpl;
+import com.example.buensaboruno.domain.dtos.ClienteDTO;
 import com.example.buensaboruno.domain.dtos.UsuarioClienteDTO;
 import com.example.buensaboruno.domain.entities.UsuarioCliente;
 import com.example.buensaboruno.presentation.base.BaseControllerImpl;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "/usuarioCliente")
 public class UsuarioClienteController extends BaseControllerImpl<UsuarioCliente, UsuarioClienteDTO, Long, UsuarioClienteFacadeImpl> {
+
+    @Autowired
+    private UsuarioClienteFacadeImpl usuarioClienteFacade;
     public UsuarioClienteController(UsuarioClienteFacadeImpl facade){
         super(facade);
     }
+    @GetMapping("/{id}/cliente")
+    public ResponseEntity<ClienteDTO> getClienteByUsuario(@PathVariable Long id) {
+        ClienteDTO clienteDTO = usuarioClienteFacade.getClienteByUsuario(id);
+        if (clienteDTO != null) {
+            return ResponseEntity.ok(clienteDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
