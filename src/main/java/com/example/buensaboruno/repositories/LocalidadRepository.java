1ac1203896e6d470773a44b72b42e5e179369c96
Lucas Cardone <lucas.a.cardone@gmail.com>
2024-06-13 18:36:40 -0300

    Arreglando DataLoader y Creacion de factura en el pedido

--- src/main/java/com/example/buensaboruno/repositories/LocalidadRepository.java
+++ src/main/java/com/example/buensaboruno/repositories/LocalidadRepository.java
@@ -1,9 +1,13 @@
 package com.example.buensaboruno.repositories;
 
 import com.example.buensaboruno.domain.entities.Localidad;
+import com.example.buensaboruno.domain.entities.Provincia;
 import com.example.buensaboruno.repositories.base.BaseRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LocalidadRepository extends BaseRepository<Localidad,Long> {
+    Optional<Localidad> findByNombreAndProvincia(String nombre, Provincia provincia);
 }
